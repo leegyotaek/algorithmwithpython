1b42c3226df9ed4e74d16aa1cade41c46610c9e1
교택 <leegyotaek@gmail.com>
2021-08-02 11:18:18 +0000

    2.find max num from list

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def sum_sq(number):
   return sum
 
 def sum_sq2(number):
-  return number * (number+1) * (2*number + 1)
+  return number * (number+1) * (2*number + 1) / 6
 
 
 # print 1*1 + 2*2 + 3*3 + 4*4....+ 10*10
@@ -15,3 +15,16 @@ print(sum_sq(10))
 print(sum_sq2(10))
 
 
+# 2. find max num out of list
+def find_max(l):
+  max_num=l[0]
+  print('max_nu : '+str(max_num))
+  n = len(l)
+
+  for i in range(n):
+    if(l[i] > max_num):
+      max_num = l[i]
+  
+  return max_num
+
+print(find_max([11,3,34100,111,10000,10001]))
